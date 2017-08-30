486c99bbe6b7e6cee65995493cf16d743893dd3f
Nathan Rollins <nathan.rollins@asu.edu>
2017-08-30 15:09:20 -0700

    Change installed ruby version to specific version number
    In order to install sass gems, the correct version of ruby must be activated using rvm. SO rather than defaulting to 'latest' we specific an exact version number to install and use.

--- image_scripts/install_sass.sh
+++ image_scripts/install_sass.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+RUBY_VERSION=${1-2.4.1}
 set -e
 main_sass() {
-  insall_sass
+  install_sass
 }
 
 # sass depenencies
-insall_sass() {
+install_sass() {
+source /etc/profile.d/rvm.sh
+rvm use "$RUBY_VERSION"
 gem install sass scss_lint
 npm install -g grunt grunt-cli
 }
